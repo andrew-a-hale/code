739de069a20737ef59874c472d3128cb082f27f5
andrew-a-hale <andrew.hale003@gmail.com>
2021-02-13 21:38:51 +1000

    catch up

---
+++ R/fold-string/fold.R
@@ -0,0 +1,32 @@
+logger::log_threshold(logger::TRACE)
+logger::log_appender(logger::appender_stdout)
+IGNORE <- structure(1000L, level = "IGNORE", class = c("loglevel", "integer"))
+
+f <- function(s) {
+  logger::log_tictoc(level = IGNORE)
+  logger::log_info("process initialising...")
+  ls <- strsplit(s, "")[[1]]
+  r <- list()
+  j <- 1
+  logger::log_info("process start!")
+  for (i in seq_along(ls)) {
+    if (identical(i %% (length(ls) %/% 10), 0)) logger::log_trace("iteration {i} of {length(ls)}")
+    if (identical(i, 1L)) {
+      r[[j]] <- list(ls[i], 1)
+    }
+    else if (ls[i] != ls[i-1]) {
+      j <- j + 1
+      r[[j]] <- list(ls[i], 1)
+    }
+    else {
+      r[[j]][[2]] <- r[[j]][[2]] + 1
+    }
+  }
+  logger::log_success("process finished!")
+  logger::log_tictoc()
+  gsub("\"", "", gsub("list", "", paste0("[", paste(r, collapse = ", "), "]")))
+}
+
+f(paste0(rep("aaaabbbccadd", 1e5), collapse = ""))
+
+
