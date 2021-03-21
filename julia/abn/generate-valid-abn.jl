af3a17176c68f10a057008a833c96d7e2aafc734
Andrew <andrew.hale003@gmail.com>
2021-03-21 12:09:32 +1000

    julia abn changes

--- julia/abn/generate-valid-abn.jl
+++ julia/abn/generate-valid-abn.jl
@@ -1,21 +1,17 @@
 module ABN
 
+## https://stackoverflow.com/questions/15503188/how-to-generate-an-australian-abn-number
 const weights = [10; range(1, 19, step = 2)]
 
 function generate_abn()::Int
-    s1 = floor.(Int, rand(9) * 10)
-    s2 = [1; 0; s1]
-    s3 = sum([popfirst!(s2) - 1; s2] .* weights)
+    ## each variable is a step of the calculation from SO thread
+    s1 = rand(0:9, 9)
+    s2 = append!([1, 0], s1)
+    s3 = sum(pushfirst!(s2, popfirst!(s2) - 1) .* weights)
     s4 = s3 % 89
     s5 = 89 - s4 + 10
-    s6 = [s5 ÷ 10; s5 % 10; s1]
-    
-    abn = 0
-    for i = 1:length(s6)
-        abn += s6[i] * 10^(length(s6) - i)
-    end
-
-    return abn
+    s6 = append!([s5 ÷ 10, s5 % 10], s1)
+    return sum(s6[i] * 10^(length(s6)-i) for i in 1:length(s6))
 end
 
 function generate_abn(n::Int) 
@@ -26,14 +22,27 @@ function generate_abn(n::Int)
     return x
 end
 
-function format_abn(abn::Int)::String
-    s = string(abn)
+function _format_abn(abn::String)
     return s[1:2] * " " * s[3:5] * " " * s[6:8] * " " * s[9:11]
 end
 
+function format_abn(abn::Int)::String
+    s = string(abn)
+    return _format_abn(s)
+end
+
+function format_abn(abn::String)::String
+    if occursin(" ", abn)
+        abn = replace(abn, " " => "")
+    end
+    return _format_abn(s)
+end
+
 function weighted_abn_sum(abn::Int)
+    ## https://abr.business.gov.au/Help/AbnFormat
+    ## see also validate_abn
     ds = reverse(digits(abn))
-    s1 = [popfirst!(ds) - 1; ds]
+    s1 = pushfirst!(ds, popfirst!(ds) - 1)
     s2 = s1 .* weights
     return sum(s2)
 end
@@ -42,7 +51,7 @@ function validate_abn(abn::Int)::Bool
     return weighted_abn_sum(abn) % 89 == 0
 end
 
-function weighted_abn_sum(abn::String)::Bool 
+function validate_abn(abn::String)::Bool 
     abn = parse(Int, replace(abn, " " => ""))
     return validate_abn(abn::Int)::Bool
 end
