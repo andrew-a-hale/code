0bb8b594d9a9f722e36c8c7626c4d43625e42f1b
Andrew <andrew.hale003@gmail.com>
2021-04-06 23:12:44 +1000

    julia sudoku

--- julia/sudoku/src/sudoku.jl
+++ julia/sudoku/src/sudoku.jl
@@ -7,22 +7,15 @@ const example_grid = "0010732802509800300986000040432009101000300050790083607000
 struct Config 
     size::Int
     rows::Int
-    value_set::Array{Int, 1}
+    value_set::Array{Int}
     subgridsize::Int
 end
 
-struct Sudoku
-    current_grid::Array{Int, 1}
-    init_grid::Array{Int, 1}
-    fail_states::Array{Int, 1}
-    changes::Array{Int, 2}
-end
-
-struct GridIndexes
-    row
-    col
-    subgrid
-    context_indexes
+mutable struct Sudoku
+    current_grid::Array{Int}
+    init_grid::Array{Int}
+    fail_states
+    changes
 end
 
 # Processing
@@ -31,18 +24,13 @@ function read_sudoku(input::String)
     if (!isinteger(input_subgridsize)) 
         error("give sudoku has unsupported dimension")
     end
-    parsed_grid::Array{Int, 1} = map(x -> parse(Int, x), split(input, ""))
-    Sudoku(parsed_grid, parsed_grid, [], [[] []])    
+    parsed_grid = map(x -> parse(Int, x), split(input, ""))
+    Sudoku(parsed_grid, parsed_grid, [], [])
 end
 
 # Precomputation
 function precomputation()
-    GridIndexes(
-        row_indexes(),
-        col_indexes(),
-        subgrid_indexes(),
-        context_indexes()
-    )
+    context_indexes()
 end
 
 # Precomputation Helpers
@@ -57,17 +45,16 @@ end
 
 function subgrid_indexes()
     function mapper(x) 
-        row_start = ((row_from_cell(x) - 1) ÷ config.subgridsize) * config.subgridsize + 1
-        col_start = ((col_from_cell(x) - 1) ÷ config.subgridsize) * config.subgridsize + 1
+        row_start = floor_nearest(row_from_cell(x), config.subgridsize)
+        col_start = floor_nearest(col_from_cell(x), config.subgridsize)
         subgrid_rows = range(row_start, step=1, length=3)
         subgrid_cols = range(col_start, step=1, length=3)
         row_indx = row_indexes()
-        map(x -> x[subgrid_cols], row_indx[subgrid_rows])
+        reduce(vcat, map(x -> x[subgrid_cols], row_indx[subgrid_rows]))
     end
     map(x -> mapper(x), range(1, length = config.size))
 end
 
-# TODO: should return an array of length 27
 function context_indexes()
     map(
         cell -> vcat(
@@ -88,20 +75,91 @@ function setup_config(sudoku::Sudoku)
 end
 
 # Solver
-function run_solver(input)
-    sudoku = read_sudoku(input)
-    config = setup_config(sudoku)
-
+function run_solver(sudoku::Sudoku)
     while !is_solved(sudoku)
         sudoku = solve(sudoku)
     end
+    sudoku
 end
 
 function solve(sudoku::Sudoku)
-    1
+    all_candidates = map(cell -> get_candidates(sudoku.current_grid, cell), range(1, length = config.size))
+    index_to_change = 0
+    for cell ∈ eachindex(all_candidates)
+        if length(all_candidates[cell]) == 1
+            index_to_change = cell
+            break
+        end
+    end
+
+    error = !isnothing(findfirst(cell -> isnothing(all_candidates[cell]) && sudoku.current_grid[cell] == 0, range(1, length = config.size)))
+
+    if index_to_change > 0
+        proposed_change = deepcopy(sudoku.current_grid)
+        proposed_change[index_to_change] = all_candidates[index_to_change] # fails here
+        if proposed_change ∈ sudoku.fail_states
+            error = true
+        end
+    end
+
+    if error
+        println(2)
+        sudoku = update_fail_states(sudoku)
+        sudoku = revert_last_change(sudoku)
+        return sudoku
+    end
+
+    if index_to_change > 0
+        sudoku.current_grid[index_to_change] = all_candidates[index_to_change]
+        sudoku = update_changes(sudoku, (index_to_change, all_candidates[index_to_change]))
+    end
+
+    if index_to_change == 0
+        cell = findfirst(iszero, sudoku.current_grid)
+        sudoku = make_guess(sudoku, cell)
+    end
+
+    solve(sudoku)
 end
 
 # Solve Subroutines
+function update_fail_states(sudoku)
+    unique!(push!(sudoku.fail_states, sudoku.current_grid))
+    sudoku
+end
+
+function update_changes(sudoku, last_change)
+    push!(sudoku.changes, last_change)
+    sudoku
+end
+
+function revert_last_change(sudoku)
+   cell, value = pop!(sudoku.changes)
+   sudoku.current_grid[cell] = value
+   sudoku
+end
+
+function get_candidates(grid, cell)
+    context = get_context(grid, cell)
+    candidates = setdiff(config.value_set, context)
+    length(candidates) > 0 ? candidates : nothing
+end
+
+function make_guess(sudoku, cell) 
+    candidates = get_candidates(sudoku.current_grid, cell)
+    for candidate ∈ candidates
+        sudoku.current_grid[cell] = candidate
+        if (sudoku.current_grid ∉ sudoku.fail_states)
+            sudoku = update_changes(sudoku, (cell, candidate))
+            return sudoku
+        end
+    end
+    # in fail state
+    sudoku.current_grid[cell] = 0
+    sudoku = update_fail_states(sudoku)
+    sudoku = revert_last_change(sudoku)
+    sudoku
+end
 
 # Validators
 function is_solved(sudoku::Sudoku) 
@@ -109,8 +167,19 @@ function is_solved(sudoku::Sudoku)
     # basic completion check
     if (0 ∈ grid)
         return false
+    elseif !validate_grid(grid)
+        return false
     end
     true
+end
+
+function validate_grid(grid)
+    all(i -> validate_context(get_context(grid, i)), range(1, length = config.size))
+end
+
+function validate_context(context) 
+    row, col, subgrid = collect(Iterators.partition(context, config.rows))
+    all(i -> length(unique(context[i])) == config.rows, [row, col, subgrid])
 end
 
 # Sudoku Helpers
@@ -120,6 +189,14 @@ end
 
 function col_from_cell(cell)
     (cell - 1) % config.rows + 1
+end
+
+function floor_nearest(x, n) 
+    (((x - 1) ÷ n) * n) + 1
+end
+
+function get_context(grid, cell)
+    grid[cell_contexts[cell]]
 end
 
 # Print
@@ -135,7 +212,7 @@ end
 
 sudoku = read_sudoku(example_grid)
 const config = setup_config(sudoku)
-grid_indexes = precomputation()
-print(sudoku)
+const cell_contexts = precomputation()
+print(run_solver(sudoku))
 
 end
