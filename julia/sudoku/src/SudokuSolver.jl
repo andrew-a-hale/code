3985fd64ead2ae055419df8cc18311000fd75c37
andrew-a-hale <57779616+andrew-a-hale@users.noreply.github.com>
2021-04-17 19:00:14 +1000

    sudoku changes?

--- julia/sudoku/src/SudokuSolver.jl
+++ julia/sudoku/src/SudokuSolver.jl
@@ -74,11 +74,11 @@ function context_indexes()
 end
 
 function setup_config(sudoku::Sudoku)
-    @time gridsize::UInt8 = length(sudoku.current_grid)
-    @time rows::UInt8 = sqrt(gridsize)
-    @time value_set::Array{UInt8} = range(0x01, length = rows)
-    @time subgridsize::UInt8 = sqrt(rows)
-    @time Config(0x00, 0x01, gridsize, rows, value_set, subgridsize)
+    gridsize::UInt8 = length(sudoku.current_grid)
+    rows::UInt8 = sqrt(gridsize)
+    value_set::Array{UInt8} = range(0x01, length = rows)
+    subgridsize::UInt8 = sqrt(rows)
+    Config(0x00, 0x01, gridsize, rows, value_set, subgridsize)
 end
 
 # Solver
@@ -108,7 +108,6 @@ function solve(sudoku::Sudoku)
     # rewrite as function
     cell_to_change::UInt8 = findfirst(!isnothing, all_candidates)
     for cell::UInt8 ∈ eachindex(all_candidates)
-        all_candidates[cell], cell
         if !isnothing(all_candidates[cell])
             if length(all_candidates[cell]) < length(all_candidates[cell_to_change])
                 cell_to_change = cell
@@ -251,13 +250,13 @@ function print(sudoku::Sudoku)
 end
 
 # Example
-#example_grid = ".....6....59.....82....8....45........3........6..3.54...325..6.................."
-#example_grid = "080070000000009300100000200002000400500080007003000900001000005009400000000010060"
-#example_grid = "100800040080010060300060100008003700000208000005400900009040000050030070030006004"
-example_grid = "900000100000003070605080003000408020800090001030107000500010908040500000002000006"
-sudoku = read_sudoku(example_grid)
-config = setup_config(sudoku)
-cell_contexts = precomputation()
-print(run_solver(sudoku))
+# const example_grid = ".....6....59.....82....8....45........3........6..3.54...325..6.................."
+# const example_grid = "080070000000009300100000200002000400500080007003000900001000005009400000000010060"
+# const example_grid = "100800040080010060300060100008003700000208000005400900009040000050030070030006004"
+const example_grid = "900000100000003070605080003000408020800090001030107000500010908040500000002000006"
+const sudoku = read_sudoku(example_grid)
+const config = setup_config(sudoku)
+const cell_contexts = precomputation()
+@time print(run_solver(sudoku))
 
 end
