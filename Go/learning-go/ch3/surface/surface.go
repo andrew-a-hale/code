f2c9224e4dc83b9911fed709a6197254be339dce
Andrew <andrew.hale003@gmail.com>
2021-05-16 12:42:40 +1000

    ch3 golang

--- Go/learning-go/ch3/surface/surface.go
+++ Go/learning-go/ch3/surface/surface.go
@@ -15,7 +15,8 @@ const (
 	xyrange       = 30                  // axis ranges (-xyranges..+xyranges)
 	xyscale       = width / 2 / xyrange // pixels per x or y unit
 	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6         // angle of x, y, axes (radians)
+	angle         = math.Pi / 8         // angle of x, y, axes (radians)
+	zfunction     = "eggbox"            // surface function
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
@@ -38,9 +39,9 @@ func makesvg(out io.Writer) io.Writer {
 			cx, cy, cz := corner(i, j+1)
 			dx, dy, dz := corner(i+1, j+1)
 			color := colorise(az, bz, cz, dz)
-			poly_points, ok := polyxml(ax, ay, bx, by, cx, cy, dx, dy, color)
+			polypointxml, ok := polycoords(ax, ay, bx, by, cx, cy, dx, dy)
 			if ok {
-				svg += poly_points
+				svg += fmt.Sprintf("<polygon points=%s style='fill:%s'/>", polypointxml, color)
 			}
 		}
 	}
@@ -50,7 +51,7 @@ func makesvg(out io.Writer) io.Writer {
 }
 
 func corner(i, j int) (float64, float64, float64) {
-	// Get (x, y, z)
+	// Get (x, y, z) for corner
 	x, y, z := xyz(i, j)
 
 	// Project (x, y, z) isometrically onto 2-D SVG canvas (sx, sy).
@@ -59,18 +60,32 @@ func corner(i, j int) (float64, float64, float64) {
 	return sx, sy, z
 }
 
+func sinronr(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance for (0, 0)
+	return math.Sin(r) / r
+}
+
 func xyz(i, j int) (float64, float64, float64) {
 	// Find point (x, y) at corner of cell (i, j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// Compute surface height z.
-	z := eggbox(x, y)
-
-	return x, y, z
+	switch zfunction {
+	case "sinronr":
+		return x, y, sinronr(x, y)
+	case "eggbox":
+		return x, y, eggbox(x, y)
+	case "saddle":
+		return x, y, saddle(x, y)
+	default:
+		return x, y, sinronr(x, y)
+	}
 }
 
+// Find (zmin, zmax) for z-function
 func surfacerange() (zmin, zmax float64) {
+	// min = +Inf, max = -Inf
 	min, max := math.Inf(1), math.Inf(-1)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
@@ -86,25 +101,20 @@ func surfacerange() (zmin, zmax float64) {
 	return min, max
 }
 
-func f(x, y float64) float64 {
-	r := math.Hypot(x, y) // distance for (0, 0)
-	return math.Sin(r) / r
-}
-
 func eggbox(x, y float64) float64 {
 	a, b := 0.1, 1.5
 	return a * (math.Sin(x/b) + math.Sin(y/b))
 }
 
 func saddle(x, y float64) float64 {
-	return math.Pow(x, 2)/height - math.Pow(y, 2)/height
+	return (math.Pow(x, 2) - math.Pow(y, 2)) / height
 }
 
-func polyxml(ax, ay, bx, by, cx, cy, dx, dy float64, color string) (value string, ok bool) {
-	if anyNaN(ay, by, cy, dy) {
+func polycoords(ax, ay, bx, by, cx, cy, dx, dy float64) (value string, ok bool) {
+	if anyNaN(ax, ay, bx, by, cx, cy, dx, dy) {
 		return "", false
 	}
-	return fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g' style='fill:%s'/>", ax, ay, bx, by, cx, cy, dx, dy, color), true
+	return fmt.Sprintf("'%g,%g %g,%g %g,%g %g,%g'", ax, ay, bx, by, cx, cy, dx, dy), true
 }
 
 func anyNaN(xs ...float64) bool {
