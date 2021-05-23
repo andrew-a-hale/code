b36c7a985c8ee36037bb63c3392c433636acd12d
Andrew <andrew.hale003@gmail.com>
2021-05-23 11:21:49 +1000

    go fizzbuzz

--- Go/fizzbuzz/fizzbuzz.go
+++ Go/fizzbuzz/fizzbuzz.go
@@ -1,45 +1,45 @@
 // Fizzbuzz
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func main() {
-	loopbuzz(100)
-	arr := makeintarray(100)
-	recursivebuzz(arr)
+	//loopbuzz(100)
+	arr := recursivebuzz(makeIntArray(100), []string{})
+	println(strings.Join(arr, "\n"))
+}
+
+func fizzBuzz(x int) string {
+	if x%15 == 0 {
+		return "FizzBuzz"
+	} else if x%3 == 0 {
+		return "Fizz"
+	} else if x%5 == 0 {
+		return "Buzz"
+	} else {
+		return strconv.Itoa(x)
+	}
 }
 
 func loopbuzz(x int) {
 	for i := 1; i < x+1; i++ {
-		if i%15 == 0 {
-			println("FizzBuzz")
-		} else if i%3 == 0 {
-			println("Fizz")
-		} else if i%5 == 0 {
-			println("Buzz")
-		} else {
-			println(i)
-		}
+		println(fizzBuzz(i))
 	}
 }
 
-func recursivebuzz(xs []int) string {
+func recursivebuzz(xs []int, done []string) []string {
 	if len(xs) == 0 {
-		return ""
+		return done
 	}
-	i := xs[0]
+	done = append(done, fizzBuzz(xs[0]))
 	xs = xs[1:]
-	if i%15 == 0 {
-		println("FizzBuzz")
-	} else if i%3 == 0 {
-		println("Fizz")
-	} else if i%5 == 0 {
-		println("Buzz")
-	} else {
-		println(i)
-	}
-	return recursivebuzz(xs)
+	return recursivebuzz(xs, done)
 }
 
-func makeintarray(x int) []int {
+func makeIntArray(x int) []int {
 	var xs []int
 	for i := 1; i <= 100; i++ {
 		xs = append(xs, i)
